0d268296373ff0c283edfe1671ea1ad9ce6e73ce
Cody McEachern <113365765+cmacspeedo@users.noreply.github.com>
2023-07-28 00:56:58 +0000

    day6

---
+++ Days1-10/Day6/maze.py
@@ -0,0 +1,19 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+while right_is_clear():
+    turn_right()
+    move()
+
+while not at_goal():
+    if right_is_clear():
+       turn_right()
+       move() 
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
